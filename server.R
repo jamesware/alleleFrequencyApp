6f58d7f22eee893a4f82913afb69ddac6777b24d
jamesware <j.ware@imperial.ac.uk>
2016-07-28 14:56:59 +0100

    update uniroot to find maximum solution

--- server.R
+++ server.R
@@ -89,26 +89,39 @@ server <- shinyServer(function(input, output) {
        }
        return(myArchitecture)
    })
+  
+   find_max_ac = function(af,an,ci=.95) {
+     # given a desired AF to filter on for Mendelian analyses, what is the max AC that would be expected in a dataset?
+     if (af == 0) {
+       return (0)
+     } else {
+       quantile_limit = ci # ci for one-sided, 1-(1-ci)/2 for two-sided
+       max_ac = qpois(quantile_limit,an*af)
+       return (max_ac)
+     }
+   }
    
-   find_af_filter <- function(ac, an, ci=.95, lower=(.1/(2*60706)), upper=2, tol=1e-7) {
+   find_af_filter = function(ac, an, ci=.95, lower=(.1/(2*60706)), upper=2, tol=1e-7, precision=1e-7) { 
      # backward method: given an observed AC, what is the highest AF filter for which one should remove this variant?
      # This function will accept an AC_Adj and an AN_Adj and will return the highest AF filter for which you would want to reject
-     # this variant as a potential Mendelian causal allele.
+     # this variant as a potential Mendelian causal allele.# for uniroot to work, lower has to be rarer than a singleton, and upper has to be higher than fixed
      # Important notes about parameters:
-     # 1. for uniroot to work, lower has to be rarer than a singleton, and upper has to be higher than fixed, hence the AF here runs from 0.1 alleles in ExAC to an impossible AF of 200%
-     # 2. you need a tight tolerance (tol) to get accurate results at low AF, so we use 1e-7 as default
-
+     # 1. for uniroot to work, lower has to be rarer than a singleton, and upper has to be higher than fixed (100% AF), 
+     #    hence the AF here runs from 0.1 alleles in ExAC to an impossible AF of 200%
+     # 2. you need a tight tolerance (tol) to get accurate results from uniroot at low AF, so we use 1e-7 as default. 
+     # 3. we also specify a precision (1e-6) for incrementing to find the upper boundary of the range of AFs for which
+     #    the 95%CI AC is less than the observed AC
+     
      if (is.na(ac) | is.na(an) | ac == 0 | an == 0 | ac == 1) {
-       return (NA)
+       return (0.0)
      } else {
        quantile_limit = ci # ci for one-sided, 1-(1-ci)/2 for two-sided
        # this has been tested and with the default parameters here, uniroot seems to never fail for an AC, AN values
        # in ExAC. still, it's good to have this tryCatch in here for debugging or in case anyone wants to override
        # the defaults. instead of just giving the error/warning message it also tells you which AC and AN values
        # it failed on.
-       # uniroot_result <- stats::uniroot(f = function(af,ac,an) { return (ac - qpois(p=quantile_limit,lambda=an*af)) },lower=lower,upper=upper,ac=ac,an=an,tol=tol)
        attempt_uniroot = tryCatch({
-         uniroot_result = uniroot(f = function(af,ac,an) { return (ac - qpois(p=quantile_limit,lambda=an*af)) },lower=lower,upper=upper,ac=ac,an=an,tol=tol)
+         uniroot_result = uniroot(f = function(af,ac,an) { return (ac - 1 - qpois(p=quantile_limit,lambda=an*af)) },lower=lower,upper=upper,ac=ac,an=an,tol=tol)
        }, warning = function(w) {
          print(paste("ac= ",as.character(ac),", an= ",as.character(an)," warning = ",as.character(w),sep=''))
          return (0.0)
@@ -117,11 +130,15 @@ server <- shinyServer(function(input, output) {
          return (0.0)
        }, finally = {
        })
-
-       return (signif(uniroot_result$root,3))
+       max_af = round(uniroot_result$root,-log10(precision)) # round to nearest millionth
+       while(find_max_ac(af=max_af,an=an) < ac) {
+         max_af = max_af + precision # increase by millionths until you find the upper bound - the highest AF for which 95%CI AC is still less than observed AC
+       }
+       max_af = max_af - precision # back off one unit from the AF that violated the 95%CI AC < obs AC condition
+       return (max_af)
      }
    }
-
+   
    output$filterAF = renderText(find_af_filter(ac=input$AC_4, an=input$AN_4, ci=as.numeric(input$CI_4), lower=(.1/(2*1000000)), upper=2, tol=1e-7))
    
    })
